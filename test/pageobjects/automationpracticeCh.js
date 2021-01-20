4f23be46fdb7d3ed3dd64eb3676db4a989775f7b
Prateek S Pannu <prateekpannu@gmail.com>
2021-01-20 11:28:37 -0500

    Added Test spec and Page Objects

--- test/pageobjects/automationpracticeCh.js
+++ test/pageobjects/automationpracticeCh.js
@@ -8,8 +8,27 @@ class automationpracticeCh extends Page  {
   get AddToCart()			{ return $('span=Add to cart'); }
   get AddToCartSuccess()	{ return $('h2*=added'); }
   
-  get ContinueShopping()	{ return $('.continue'); }
   get ShoppingCartLink()	{ return $('.shopping_cart a'); }
+  get CartBlock()			{ return $('.cart_block'); }
+  get Cartprices()			{ return $('.cart-prices'); }
+  get CartOrder()			{ return $('#button_order_cart'); }
+  get QvClose()				{ return $('#layer_cart').$('.cross'); }
+  
+  get ProceedCheckOut()		{ return $('.standard-checkout'); }
+  get adrProceedCheckOut()	{ return $('[method="post"]').$('span*=Proceed'); }
+  get StepOneCurrent()		{ return $('.step_current.first'); }
+  get StepTwoCurrent()		{ return $('.step_current.second'); }
+  get StepThreeCurrent()	{ return $('.step_current.third'); }
+  get StepFourCurrent()		{ return $('.step_current.four'); }
+  get StepLastCurrent()		{ return $('.step_current.last'); }
+  
+  get EmailField()			{ return $('#email'); }
+  get PasswordField()		{ return $('#passwd'); }
+  get login()				{ return $('#SubmitLogin'); }
+  
+  get TermsCheckBox()		{ return $('.checker'); }
+  get Product()				{ return $('small*=SKU'); }
+  get total_price()			{ return $('span*=$'); }
   
   /**
    * define or overwrite page methods
@@ -33,10 +52,80 @@ class automationpracticeCh extends Page  {
 	this.ShoppingCartLink.moveTo();
   }
   
-  selectContinueShopping () {
-	this.ContinueShopping.click();
+  selectCartOrder () {
+	this.ShoppingCartLink.waitForDisplayed(1000);
+	this.ShoppingCartLink.moveTo();
+	browser.pause(1000);
+	this.CartOrder.click();
   }
   
+  selectQvClose () {
+	this.QvClose.click();
+  }
+  
+  isQvClose () {
+	return this.QvClose.isFocused();
+  }
+  
+  selectTermsCheckBox() {
+	this.TermsCheckBox.waitForDisplayed(1000);
+	this.TermsCheckBox.click();
+  }
+  
+  selectProceedCheckOut () {
+	this.ProceedCheckOut.waitForDisplayed(1000);
+	this.ProceedCheckOut.click();
+  }
+  
+  selectadrProceedCheckOut () {
+	this.adrProceedCheckOut.waitForDisplayed(1000);
+	this.adrProceedCheckOut.click();
+  }
+  
+  whatIsProduct () {
+	return this.Product.getText();
+  }
+  
+  isTotalQuoted () {
+	return this.total_price.isExisting();
+  }
+  
+  isStepOneCurrent () {
+	 this.StepOneCurrent.waitForDisplayed(1000);
+	 return this.StepOneCurrent.isExisting();
+  }
+  
+  isStepTwoCurrent () {
+	 this.StepTwoCurrent.waitForDisplayed(1000);
+	 return this.StepTwoCurrent.isExisting();
+  }
+  
+  isStepThreeCurrent () {
+	 this.StepThreeCurrent.waitForDisplayed(1000);
+	 return this.StepThreeCurrent.isExisting();
+  }
+  
+  isStepFourCurrent () {
+	 this.StepFourCurrent.waitForDisplayed(1000);
+	 return this.StepFourCurrent.isExisting();
+  }
+  
+  isStepLastCurrent () {
+	 this.StepLastCurrent.waitForDisplayed(1000);
+	 return this.StepLastCurrent.isExisting();
+  }
+  
+  inputEmailField () {
+	 this.EmailField.setValue('automationpractice00000@automationpractice.com');
+  }
+  
+  inputPasswordField () {
+	 this.PasswordField.setValue('AutomationPractice000000');
+  }
+  
+  selectLogin () {
+	 this.login.click();
+  }
 }
 
 export default new automationpracticeCh();
--- test/specs/automationpracticeCheckout.spec.js
+++ test/specs/automationpracticeCheckout.spec.js
@@ -5,18 +5,57 @@ import assert       			from 'assert';
 	This is a BDD test using Mocha JavaScript framework
 */
 
-describe('Navigating to checkout', function() {
+describe('Navigating to desired state', function() {
   
-  it('Should open desired url', function () {
-    automationpracticeCh.open();
-	assert.equal(browser.getTitle(), 'Printed Summer Dress - My Store');
-	automationpracticeCh.selectAddToCart();
-	assert.equal(automationpracticeCh.AddToCartSuccessTxt(), "Product successfully added to your shopping cart");
-	automationpracticeCh.selectContinueShopping();
-  });
+	it('Should open to desired url', function () {
+		automationpracticeCh.open();
+		assert.equal(browser.getTitle(), 'Printed Summer Dress - My Store');
+	});
   
-  it('Should Hover over to "Cart" section and click "Check Out" button', function (){
-	  automationpracticeCh.selectShoppingCart();
-  })
-  
+	it('Should add product to cart', function () {
+		automationpracticeCh.selectAddToCart();
+		assert.equal(automationpracticeCh.AddToCartSuccessTxt(), "Product successfully added to your shopping cart");
+	});
+	
+	it('Should go back to product', function () {
+		automationpracticeCh.selectQvClose();
+		assert.equal(automationpracticeCh.isQvClose(), false);
+	});
 });
+
+describe('Navigating to checkout', function() {
+	it('Should Hover over to "Cart" section and click "Check Out" button', function (){
+		automationpracticeCh.selectShoppingCart();
+		automationpracticeCh.selectCartOrder();
+		assert.equal(browser.getTitle(), 'Order - My Store');
+	});
+	
+	it('Should Click Proceed to checkout button', function() {
+		automationpracticeCh.selectProceedCheckOut();
+		assert.equal(automationpracticeCh.isStepTwoCurrent(), true);
+	});
+	
+	it('Should Should Sign-in User', function() {
+		automationpracticeCh.inputEmailField();
+		automationpracticeCh.inputPasswordField();
+		automationpracticeCh.selectLogin();
+		assert.equal(automationpracticeCh.isStepThreeCurrent(), true);
+	});
+	it('Should Click Proceed to checkout button on Address tab', function() {
+		automationpracticeCh.selectadrProceedCheckOut();
+		assert.equal(automationpracticeCh.isStepFourCurrent(), true);
+	});
+	
+	it('Agree to Terms of service and Click on Proceed to checkout button on Shipping tab', function() {
+		automationpracticeCh.selectTermsCheckBox();
+		automationpracticeCh.selectadrProceedCheckOut();
+		assert.equal(automationpracticeCh.isStepLastCurrent(), true);
+	});
+	
+	it('Confirm the correct order on Payment tab', function() {
+		assert.equal(automationpracticeCh.whatIsProduct(), 'SKU : demo_6');
+		assert.equal(automationpracticeCh.isTotalQuoted(), true);
+		
+	});
+  
+})
